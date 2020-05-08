f404cc891a85385924088456ea53b72b165358df
Dokku <vsdokku@cgifederal.com>
2020-05-07 20:31:28 -0400

    First commit

---
+++ src/main/java/com/learn/SpringCloudStreamKafkaStreams/SpringCloudStreamKafkaStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.learn.SpringCloudStreamKafkaStreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamKafkaStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamKafkaStreamsApplication.class, args);
+	}
+
+}
