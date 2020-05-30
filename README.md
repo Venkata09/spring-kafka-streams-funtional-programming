7a581ae5a510d66bb20ed2c010833537d8505255
Dokku <vsdokku@cgifederal.com>
2020-05-29 22:50:49 -0400

    **** Coding Practice

--- README.md
+++ README.md
@@ -1,3 +1,96 @@
 # spring-kafka-streams-funtional-programming
 
 This is the repo for learning the spring cloud stream for kafka streams. 
+
+Behind the scenes, the kafka stream binder for spring cloud stream will convert this into a proper kafka streams application with a **StreamBuilder**, 
+
+One of the prime tenets for spring cloud stream is to hide the complexity and boilerplate away from the user so that the application developer can focus on the 
+bussiness logic. 
+
+Binder will take care of creating the KAFKA STREAM topology, connecting to a kafka cluster, binding to a topic and consuming data from that 
+topic which is bound as KStream in this case. 
+
+Kafka-Stream requires 
+1. Cluster information
+    ```text
+   
+    By default the binder will try to connect a cluster that is running on localhost:9092.
+    
+   ```
+1. Application id
+
+```java
+@Bean
+    public java.util.function.Consumer<KStream<String, String>> process() {
+        return input -> input.foreach((key, value) -> {
+            System.out.println("Key::::" + key + "----> Value ::::" + value);
+        });
+    }
+```
+
+```java
+
+In a Kafka Streams application, application.id is a mandatory field. Without it, you cannot start a Kafka Streams application. 
+
+By default, the binder will generate an application ID and assign it to the processor.
+
+
+It uses the function bean name as a prefix. For e.g, if you have a consumer as above, the binder will generate the application ID as process-applicationId. 
+```
+
+1. topic to consume
+
+```text
+
+spring.cloud.stream.bindings.process-in-0.destination: my-input-topic
+
+```
+ 
+1. Serde to use 
+
+```java
+
+
+
+
+Kafka Streams uses a special class called Serde to deal with data marshaling. It is essentially a wrapper around a deserializer on the inbound and a serializer on the outbound. 
+
+Binder, however, infers this information by using the parametric types provided as part of Kafka Streams. For example, in the case of KStream<String, String>, the binder assumes that it needs to use String deserializers.
+
+
+
+
+
+
+
+
+
+
+```
+
+
+
+# Scenario - 1: Single input and output binding
+
+From Spring perspective the binding is not mapped to a single TOPIC.
+
+That means a single binding can be applied to multiple topics. 
+
+##### topics could be multiplexed and attached to a single input binding (with comma-separated multiple topics configured on a single binding
+
+
+
+Ex: spring.cloud.stream.bindings.wordcount-in-0.destination=words
+
+the above setting or configuration can be modifed to multiple topics as well.
+
+spring.cloud.stream.bindings.wordcount-in-0.destination=words1,words2,word3
+
+The output stream is configured as follows: spring.cloud.stream.bindings.wordcount-out-0.destination=counts
+
+
+
+
+# Scenario 2: Multiple output bindings through Kafka Streams branching
+
+
--- src/test/java/com/learn/SpringCloudStreamKafkaStreams/SpringCloudStreamKafkaStreamsApplicationTests.java
+++ src/test/java/com/learn/SpringCloudStreamKafkaStreams/SpringCloudStreamKafkaStreamsApplicationTests.java
@@ -1,13 +1,79 @@
 package com.learn.SpringCloudStreamKafkaStreams;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.config.StreamsBuilderFactoryBean;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        properties = {"server.port=0"})
 class SpringCloudStreamKafkaStreamsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @ClassRule
+    public static EmbeddedKafkaRule embeddedKafkaRule = new EmbeddedKafkaRule(1, true, "words", "counts");
+
+    private static EmbeddedKafkaBroker embeddedKafka = embeddedKafkaRule.getEmbeddedKafka();
+
+    private static Consumer<String, String> consumer;
+
+    @Autowired
+    StreamsBuilderFactoryBean streamsBuilderFactoryBean;
+
+    @Before
+    public void before() {
+        streamsBuilderFactoryBean.setCloseTimeout(0);
+    }
+
+    @BeforeClass
+    public static void setUp() {
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("group", "false", embeddedKafka);
+        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        DefaultKafkaConsumerFactory<String, String> cf = new DefaultKafkaConsumerFactory<>(consumerProps);
+        consumer = cf.createConsumer();
+        embeddedKafka.consumeFromAnEmbeddedTopic(consumer, "counts");
+        System.setProperty("spring.cloud.stream.kafka.streams.binder.brokers", embeddedKafka.getBrokersAsString());
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        consumer.close();
+        System.clearProperty("spring.cloud.stream.kafka.streams.binder.brokers");
+    }
+
+    @Test
+    public void testKafkaStreamsWordCountProcessor() {
+        Map<String, Object> senderProps = KafkaTestUtils.producerProps(embeddedKafka);
+        DefaultKafkaProducerFactory<Integer, String> pf = new DefaultKafkaProducerFactory<>(senderProps);
+        try {
+            KafkaTemplate<Integer, String> template = new KafkaTemplate<>(pf, true);
+            template.setDefaultTopic("words");
+            template.sendDefault("foobar");
+            ConsumerRecords<String, String> cr = KafkaTestUtils.getRecords(consumer);
+            assertThat(cr.count()).isGreaterThanOrEqualTo(1);
+        } finally {
+            pf.destroy();
+        }
+    }
 
 }
